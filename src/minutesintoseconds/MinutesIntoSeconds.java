314a4240e75e33129e6c06ab59fd75ac7811b171
K Hareesh <hareesh.komma@improve10x.com>
2023-07-17 12:25:08 +0530

    I completed MinutesIntoSeconds problem

---
+++ src/minutesintoseconds/MinutesIntoSeconds.java
@@ -0,0 +1,9 @@
+package minutesintoseconds;
+
+public class MinutesIntoSeconds {
+    public int convertMinIntoSeconds(int minutes) {
+        int noOfSeconds = 0;
+        noOfSeconds = minutes * 60;
+        return noOfSeconds;
+    }
+}
--- src/minutesintoseconds/MinutesIntoSecondsTest.java
+++ src/minutesintoseconds/MinutesIntoSecondsTest.java
@@ -2,7 +2,11 @@ package minutesintoseconds;
 
 import org.testng.annotations.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MinutesIntoSecondsTest {
+
+    private MinutesIntoSeconds minutesIntoSeconds = new MinutesIntoSeconds();
 
     @Test
     public void nothing() {
@@ -15,6 +19,19 @@ public class MinutesIntoSecondsTest {
      */
     @Test
     public void givenThree_returnsOneEightyseconds() {
+        int minIntoSeconds = minutesIntoSeconds.convertMinIntoSeconds(3);
+        assertEquals(180, minIntoSeconds);
+    }
 
+    @Test
+    public void givenTwo_returnsOneTwentySeconds() {
+        int minIntoSeconds = minutesIntoSeconds.convertMinIntoSeconds(2);
+        assertEquals(120, minIntoSeconds);
+    }
+
+    @Test
+    public void givenTwentyOne_returns1260() {
+        int minIntoSeconds = minutesIntoSeconds.convertMinIntoSeconds(21);
+        assertEquals(1260, minIntoSeconds);
     }
 }

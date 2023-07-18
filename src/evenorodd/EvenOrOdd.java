a3717d5dcab47c306c9e7d9618f183436b04df79
K Hareesh <hareesh.komma@improve10x.com>
2023-07-18 11:33:16 +0530

    I Completed the IntegerInRange problem using TDD approach

---
+++ src/evenorodd/EvenOrOdd.java
@@ -0,0 +1,9 @@
+package evenorodd;
+
+public class EvenOrOdd {
+    public String findEvenOdd(int n) {
+        String result = "";
+        return result;
+
+    }
+}
--- src/evenorodd/EvenOrOddTest.java
+++ src/evenorodd/EvenOrOddTest.java
@@ -3,8 +3,24 @@ package evenorodd;
 import org.testng.annotations.Test;
 import org.testng.xml.dom.ParentSetter;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class EvenOrOddTest {
 
     @Test
     public void nothing(){}
+
+    /*
+    evenOrOdd(3) → odd
+    evenOrOdd(2) → even
+    evenOrOdd(15) → odd
+    evenOrOdd(24) → even
+
+     */
+    @Test
+    public void givenThree_returnsOdd() {
+        EvenOrOdd evenOrOdd = new EvenOrOdd();
+        String givenThree = evenOrOdd.findEvenOdd(3);
+        assertEquals("Odd", givenThree);
+    }
 }

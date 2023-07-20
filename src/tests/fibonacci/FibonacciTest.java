cfb862e5a6e646d7ac24d95978f727a03b430431
K Hareesh <hareesh.komma@improve10x.com>
2023-07-20 11:59:23 +0530

    I wrote Fibonacci negativeNumber test

--- src/tests/fibonacci/FibonacciTest.java
+++ src/tests/fibonacci/FibonacciTest.java
@@ -2,8 +2,17 @@ package fibonacci;
 
 import org.testng.annotations.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FibonacciTest {
 
     @Test
     public void nothing() {}
+
+    @Test
+    public void givenNegativeNumber_returnsNegativeNumber() {
+        Fibonacci fibonacci = new Fibonacci();
+        int negativeFibonacci = fibonacci.findNthNumber(-42);
+        assertEquals(-1, negativeFibonacci);
+    }
 }

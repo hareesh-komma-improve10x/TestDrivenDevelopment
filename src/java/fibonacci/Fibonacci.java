31055b818520354626a5feb7792eed9698c7a37c
K Hareesh <hareesh.komma@improve10x.com>
2023-07-20 12:20:33 +0530

    I refactor some code

--- src/java/fibonacci/Fibonacci.java
+++ src/java/fibonacci/Fibonacci.java
@@ -8,7 +8,8 @@ public class Fibonacci {
             return 0;
         } else if (n == 2) {
             return 1;
+        } else {
+            return findNthNumber(n - 1) + findNthNumber(n - 2);
         }
-        return n;
     }
 }
--- src/tests/fibonacci/FibonacciTest.java
+++ src/tests/fibonacci/FibonacciTest.java
@@ -1,40 +1,50 @@
 package fibonacci;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.testng.annotations.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FibonacciTest {
 
+    private Fibonacci fibonacci = new Fibonacci();
+
     @Test
     public void nothing() {}
 
     @Test
     public void givenNegativeNumber_returnsNegativeNumber() {
-        Fibonacci fibonacci = new Fibonacci();
         int negativeFibonacci = fibonacci.findNthNumber(-42);
         assertEquals(-1, negativeFibonacci);
     }
 
     @Test
     public void givenZero_returnsNegativeNumber() {
-        Fibonacci fibonacci = new Fibonacci();
         int negativeFibonacci = fibonacci.findNthNumber(0);
         assertEquals(-1, negativeFibonacci);
     }
 
     @Test
     public void givenOne_returnsZero() {
-        Fibonacci fibonacci = new Fibonacci();
         int firstFibonacci = fibonacci.findNthNumber(1);
         assertEquals(0, firstFibonacci);
     }
 
     @Test
-    public void givenTwo_returnsZero() {
-        Fibonacci fibonacci = new Fibonacci();
+    public void givenTwo_returnsOne() {
         int secondFibonacci = fibonacci.findNthNumber(2);
         assertEquals(1, secondFibonacci);
     }
 
+    @Test
+    public void givenThree_returnsOne() {
+        int threeFibonacci = fibonacci.findNthNumber(3);
+        assertEquals(1, threeFibonacci);
+    }
+
+    @Test
+    public void givenSix_returnsThree() {
+        int fiveFibonacci = fibonacci.findNthNumber(5);
+        assertEquals(3, fiveFibonacci);
+    }
 }

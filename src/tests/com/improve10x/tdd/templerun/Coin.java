0d909730814e39c0015b2d78583431c43b22a3c9
K Hareesh <hareesh.komma@improve10x.com>
2023-07-24 16:48:17 +0530

    I wrote the test case for given10_whenGetValueCalled_thenReturn10 and given20_whenGetValueCalled_thenReturn20

--- src/tests/com/improve10x/tdd/templerun/Coin.java
+++ src/tests/com/improve10x/tdd/templerun/Coin.java
@@ -1,8 +1,19 @@
 package com.improve10x.tdd.templerun;
 
 public class Coin {
+
+    private int value;
+
     public Coin(int value) {
-        throw new InvalidCoinValueException();
+        if (value < 0 || value > 100) {
+            throw new InvalidCoinValueException();
+        } else {
+            this.value = value;
+        }
+    }
+
+    public int getValue() {
+        return value;
     }
 
     public class InvalidCoinValueException extends RuntimeException{
--- src/tests/com/improve10x/tdd/templerun/CoinTest.java
+++ src/tests/com/improve10x/tdd/templerun/CoinTest.java
@@ -2,6 +2,7 @@ package com.improve10x.tdd.templerun;
 
 import org.testng.annotations.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CoinTest {
@@ -16,5 +17,15 @@ public class CoinTest {
     public void given101_throwsInvalidCoinValueException() {
         assertThrows(Coin.InvalidCoinValueException.class, () -> new Coin(101),
                 "Coin value should be between 0 to 100");
+    }
+
+    @Test
+    public void given10_whenGetValueCalled_thenReturn10() {
+        assertEquals(10, new Coin(10).getValue());
+    }
+
+    @Test
+    public void given20_whenGetValueCalled_thenReturn20() {
+        assertEquals(20, new Coin(20).getValue());
     }
 }

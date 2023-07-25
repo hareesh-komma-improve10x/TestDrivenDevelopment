6e9fdb1f4b0fe8edd16d7d6783f847a5928c60a3
K Hareesh <hareesh.komma@improve10x.com>
2023-07-25 12:50:04 +0530

    I wrote test case for given100_whenRollMethodCalled_displayRollingMessage

--- src/java/com/improve10x/tdd/templerun/FireBall.java
+++ src/java/com/improve10x/tdd/templerun/FireBall.java
@@ -2,13 +2,17 @@ package com.improve10x.tdd.templerun;
 
 public class FireBall {
 
-    public int speed;
+    private int speed;
 
     public FireBall(int speed) {
         this.speed = speed;
         if (speed < 0 || speed > 100) {
             throw new InvalidFireBallSpeedException();
         }
+    }
+
+    public void roll() {
+        System.out.println("FireBall is rolled with " + speed + " kmh speed!");
     }
 
     public class InvalidFireBallSpeedException extends RuntimeException{
